d511a0b40dd4e0b87c5d651599544f8bff3620d3
Naga Shourya <nagashouryatolearn@outlook.com>
2020-02-17 16:38:14 +0530

    updated nagashourya.py

---
+++ nagashourya.py
@@ -0,0 +1,22 @@
+Hello Shourya
+How are you?
+
+Hope you are behaving well!! at school
+
+I trust you!!
+
+I know you will become a great person in future
+
+All the very best your future....
+
+May god bless you with all the happiness in the world.....
+
+Stay blessed my dear lovely son......
+
+I love you so much... hope you too love me.. for ever and ever....
+
+Take care of yourself...be a strong boy... 
+
+I want you to be very strong in all the aspects of life
+
+
